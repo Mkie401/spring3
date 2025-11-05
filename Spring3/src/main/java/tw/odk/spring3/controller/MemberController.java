464dc6beb68c0b18179a0185e05dae1e635098d6
Mkie401 <b23851520@gmail.com>
2025-11-05 11:59:22 +0800

    11051159

--- Spring3/src/main/java/tw/odk/spring3/controller/MemberController.java
+++ Spring3/src/main/java/tw/odk/spring3/controller/MemberController.java
@@ -1,12 +1,18 @@
 package tw.odk.spring3.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import tw.odk.spring3.entity.Member;
 import tw.odk.spring3.service.MemberService;
 
 @RestController
@@ -27,5 +33,20 @@ public class MemberController {
 		boolean isExist = service.checkAccount(account);
 		return ResponseEntity.ok(isExist);
 	}
+	
+	
+	/* request: Member => {}
+	 * response: {"success":"true/false"} 
+	 */
+	@PostMapping("/register")
+	public ResponseEntity<Map<String, Boolean>> register(@RequestBody Member member) {
+		
+		Map<String, Boolean> map = new HashMap<String, Boolean>();
+		
+		map.put("success", service.register(member));
+		
+		return ResponseEntity.ok(map);
+	}
+	
 	
 }
--- Spring3/src/main/java/tw/odk/spring3/service/MemberService.java
+++ Spring3/src/main/java/tw/odk/spring3/service/MemberService.java
@@ -1,9 +1,12 @@
 package tw.odk.spring3.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
+import tw.odk.spring3.entity.Member;
 import tw.odk.spring3.repository.MemberRepository;
+import tw.odk.spring3.utils.BCrypt;
 
 @Service
 public class MemberService {
@@ -14,4 +17,16 @@ public class MemberService {
 	public boolean checkAccount(String account) {
 		return repository.existsByAccount(account);
 	}
+	
+	public boolean register(Member member) {
+		if (repository.existsByAccount(member.getAccount())) {	
+			return false;
+		}
+		member.setPasswd(BCrypt.hashpw(member.getPasswd(), BCrypt.gensalt()));
+		Member saveMember = repository.save(member);
+		return saveMember != null;
+	}
+	
+	
+	
 }
